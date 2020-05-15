be544744e3ba6856485e11b94e7d16c9903d5e83
Halley Tsai <hftsai256@gmail.com>
2020-05-14 22:32:36 -0400

    add Ubuntu support with neovim-qt as default GUI frontend

--- bootstrap/modules/nvim.sh
+++ bootstrap/modules/nvim.sh
@@ -6,7 +6,7 @@ if [[ $(util_getos) =~ osx ]]; then
     NVIM_CFG_PATH="${HOME}/.config/nvim"
     p_arrow "Looking for installed neovim"
 
-    # Check if sublime has been installed under /usr/local/bin
+    # Check if neovim has been installed under /usr/local/bin
     if [[ ! -f /usr/local/bin/nvim ]]; then
         p_arrow "Install neovim with Homebrew"
         brew install neovim
@@ -20,7 +20,7 @@ if [[ $(util_getos) =~ osx ]]; then
     # Install python 3 neovim support package
     if $(command -v pip3 >/dev/null 2>&1 ); then
         p_arrow "Install neovim python packages"
-        sudo pip3 install -qqq neovim
+        sudo pip3 install -qqq pynvim
     fi
 
     # Install Node.js and neovim support package
@@ -43,6 +43,19 @@ fi
 if [[ $(util_getos) =~ ubuntu ]]; then
     NVIM_CFG_PATH="${HOME}/.config/nvim"
     p_arrow "Looking for installed neovim"
+
+    # Check if neovim has been installed
+	which nvim > /dev/null 2>&1 || sudo apt install -y neovim
+	which nvim-qt > /dev/null 2>&1 || sudo apt install -y neovim-qt 
+	which vimr > /dev/null 2>&1 || sudo ln -s /usr/bin/nvim-qt /usr/bin/vimr
+
+	# Install python3 neovim support package
+	which pip3 > /dev/null 2>&1 && sudo pip3 -H install -qqq pynvim
+
+	# Install node.js for neovim
+	curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+	sudo apt-get install -y nodejs
+	sudo npm install -g neovim
 fi
 
 p_success "Done"
