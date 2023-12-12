6a3c50e1655dd7a783359c3de0a359180c5762fc
Halley Tsai <hftsai256@gmail.com>
2023-12-12 13:15:54 -0500

    revert swayfx

---
+++ xdg_config/sway/scripts/ssh-agent.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+pgrep -u "$USER" ssh-agent > /dev/null || {
+    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
+}
+
+[[ ! -f "$SSH_AUTH_SOCK" ]] || {
+    source "$XDG_RUNTIME_DIR/ssh-agent.env"
+}
+
+for key in $HOME/.ssh/{id_*,*_rsa}; do
+    ssh-add -k $key 2>&1 | tee -a $HOME/ssh-agent-add.log; 
+done
+
