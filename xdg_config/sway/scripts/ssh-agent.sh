b530a671a365bd3d5e982cce8a7f9fa1076300d1
Halley Tsai <htsai@cytonome.com>
2024-04-25 13:30:07 -0400

    use systemd ssh-agent service

--- xdg_config/sway/scripts/ssh-agent.sh
+++ xdg_config/sway/scripts/ssh-agent.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-pgrep -u "$USER" ssh-agent > /dev/null || {
-    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
-}
-
-[[ ! -f "$SSH_AUTH_SOCK" ]] || {
-    source "$XDG_RUNTIME_DIR/ssh-agent.env"
-}
+SSH_AUTH_SOCK=/run/user/1000/ssh-agent.socket
 
 for key in $HOME/.ssh/{id_*,*_rsa}; do
+    echo add key $key to ssh agent keyring
     ssh-add -k $key
 done
 
