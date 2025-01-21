36c5e61b8c358f8e5158b8615e74385be6ce379f
Halley Tsai <hftsai256@gmail.com>
2025-01-21 14:09:53 -0500

    switch Linux kernel line

--- nix/modules/nixos/boot.nix
+++ nix/modules/nixos/boot.nix
@@ -1,14 +1,28 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 {
-  boot = {
+  options = {
+    kernel.type = lib.mkOption {
+      type = lib.types.enum [ "lts" "mainline" "rt" ];
+      default = "lts";
+      description = "Linux kernel variants";
+    };
+  };
+
+  config.boot = let
+    kernel = with pkgs; {
+      lts = linuxPackages;
+      mainline = linuxPackages_6_12;
+      rt = linuxPackages-rt;
+    };
+
+  in {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = kernel.${config.kernel.type};
     supportedFilesystems = {
       btrfs = true;
       ntfs = true;
       zfs = lib.mkForce false;
-    }
-    ;
+    };
   };
 }
