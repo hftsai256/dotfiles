4c3f64f643ec482a915d7ef6e1ca4766bf2339ca
Halley Tsai <hftsai256@gmail.com>
2026-05-07 19:52:19 -0400

    Intel CNVi Bluetooth workaround

--- nix/modules/nixos/bluetooth.nix
+++ nix/modules/nixos/bluetooth.nix
@@ -16,4 +16,15 @@
   environment.systemPackages = [
     pkgs.blueman
   ];
+
+  # Restart Bluetooth after hibernate/suspend-resume on Intel CNVi (XPS 9315)
+  systemd.services."bluetooth-resume" = {
+    description = "Restart Bluetooth after suspend/hibernate";
+    wantedBy = [ "suspend.target" "hibernate.target" ];
+    after = [ "suspend.target" "hibernate.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/systemctl restart bluetooth.service";
+    };
+  };
 }
