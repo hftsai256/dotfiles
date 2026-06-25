a2eadd9a979c6236f6f9bc0bbe5ec5071f35dd48
Halley Tsai <hftsai256@gmail.com>
2026-06-24 22:08:20 -0400

    switch to shikane: CYT

--- nix/users/hftsai-CYT-HTSAI-LINUX.nix
+++ nix/users/hftsai-CYT-HTSAI-LINUX.nix
@@ -13,94 +13,72 @@
   guiApps.cadLab.enable = false;
   qs.enable = true;
 
-  kanshi.settings = let
-    homeRes = { x = 3840; y = 2160; r = 1.5; };
+  shikane.settings = let
+    homeRes   = { x = 3840; y = 2160; r = 1.5; };
     officeRes = { x = 3840; y = 2160; r = 1.5; };
     laptopRes = { x = 1920; y = 1200; r = 1.2; };
-
+    builtinDisplay = "n=eDP-1";
     scale = res: {
       inherit (res) r;
       x = builtins.floor (res.x / res.r);
       y = builtins.floor (res.y / res.r);
     };
 
-  in [
-    { profile.name = "office";
-      profile.outputs = [
-      {
-        criteria = "Dell Inc. DELL P2723QE 24QVXV3";
-        status = "enable";
-        scale = officeRes.r;
-        position = "${toString (scale laptopRes).x},0";
+  in {
+    profile = [
+      { name = "office";
+        output = [
+          { search = ["m=DELL P2723QE" "s=24QVXV3" "v=Dell Inc."];
+            enable = true;
+            scale = officeRes.r;
+            position = "${toString (scale laptopRes).x},0";
+          }
+          { search = builtinDisplay;
+            enable = true;
+            scale = laptopRes.r;
+            position = "0,0";
+          }
+        ];
       }
-      {
-        criteria = "Sharp Corporation 0x1548 Unknown";
-        status = "enable";
-        scale = laptopRes.r;
-        position = "0,0";
+      { name = "home";
+        output = [
+          { search = ["m=DELL S2721QS" "s=FYCXM43" "v=Dell Inc."];
+            enable = true;
+            scale = homeRes.r;
+            position = "0,0";
+          }
+          { search = builtinDisplay;
+            enable = true;
+            scale = laptopRes.r;
+            position = let
+              dx = ((scale homeRes).x - (scale laptopRes).x) / 2;
+              dy = (scale homeRes).y;
+            in "${toString dx},${toString dy}";
+          }
+        ];
       }
-    ]; }
-
-    { profile.name = "home";
-      profile.outputs = [
-      {
-        criteria = "Dell Inc. DELL S2721QS FYCXM43";
-        status = "enable";
-        scale = homeRes.r;
-        position = "0,0";
+      { name = "clamshell";
+        output = [
+          { search = "n/(DP|HDMI)-[1-9]$";
+            enable = true;
+            scale = homeRes.r;
+            position = "0,0";
+          }
+          { search = builtinDisplay;
+            enable = false;
+          }
+        ];
       }
-      {
-        criteria = "Sharp Corporation 0x1548 Unknown";
-        status = "enable";
-        scale = laptopRes.r;
-        position =
-          let
-            dx = ((scale homeRes).x - (scale laptopRes).x) / 2;
-            dy = (scale homeRes).y;
-          in
-            "${toString dx},${toString dy}";
+      { name = "standalone";
+        output = [
+          { search = builtinDisplay;
+            enable = true;
+            scale = laptopRes.r;
+            position = "0,0";
+          }
+        ];
       }
-    ]; }
-
-    { profile.name = "clamshell";
-      profile.outputs = [
-      {
-        criteria = "Dell Inc. DELL S2721QS FYCXM43";
-        status = "enable";
-        scale = homeRes.r;
-        position = "0,0";
-      }
-      {
-        criteria = "Sharp Corporation 0x1548 Unknown";
-        status = "disable";
-      }
-    ]; }
-
-    { profile.name = "generic";
-      profile.outputs = [
-      {
-        criteria = "*";
-        status = "enable";
-        scale = officeRes.r;
-        position = "${toString (scale laptopRes).x},0";
-      }
-      {
-        criteria = "Sharp Corporation 0x1548 Unknown";
-        status = "enable";
-        scale = 1.2;
-        position = "0,0";
-      }
-      ]; }
-
-    { profile.name = "standalone";
-      profile.outputs = [
-      {
-        criteria = "Sharp Corporation 0x1548 Unknown";
-        status = "enable";
-        scale = laptopRes.r;
-        position = "0,0";
-      }
-    ]; }
-  ];
+    ];
+  };
 }
 
