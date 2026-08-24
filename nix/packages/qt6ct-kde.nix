0f7aea26e4be710fcfd1d074ceab2525ddccd0e9
Halley Tsai <hftsai256@gmail.com>
2026-08-24 10:07:44 -0400

    update qt6ct patch hash

--- nix/packages/qt6ct-kde.nix
+++ nix/packages/qt6ct-kde.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   patches = [
     (fetchurl {
       url = "https://aur.archlinux.org/cgit/aur.git/plain/qt6ct-shenanigans.patch?h=qt6ct-kde";
-      hash = "sha256-EHiyx1SuX2D4EGiX/erQhFTSHNmjuMD2ezLz6gQLqqE=";
+      hash = "sha256-uqsrcUrUkN46Eu3V1OwPYiPt7QNNZqaUmJ50a4bR9CA=";
     })
   ];
 
